0961afdaab22ef341d20adc86b4a853a676fea7a
SX-flyinG <outcast9n0k@gmail.com>
2025-01-10 18:01:24 +0200

    Update potok228.cpp

--- potok228/potok228.cpp
+++ potok228/potok228.cpp
@@ -4,18 +4,68 @@
 using namespace std;
 
 class Auto {
-
-public:
 	string brand;
 	int speed;
 	int year;
 	int ves;
+public:
 
 	Auto() {
 		brand = "Audi RS8";
 		speed = 240;
 		year = 2021;
 		ves = 4;
+	}
+
+	Auto(string brand, int speed, int year, int ves) {
+		this->brand = brand;
+		this->speed = speed;
+		this->year = year;
+		this->ves = ves;
+	}
+
+	string getBrand() {
+		return brand;
+	}
+
+	int getSpeed() {
+		return speed;
+	}
+	int getVes() {
+		return ves;
+	}
+
+	int getYear() {
+		return ves;
+	}
+
+	void writeOutFile(Auto& a){
+		ofstream outFile("auto.txt");
+		if (outFile.is_open()) {
+			outFile << "Brand: " << a.getBrand()  << "\nSpeed :  " << a.getSpeed() << "\nYear of presentation : " << a.getYear() << "\nVes : " << a.getVes() << endl;;
+			outFile.close();
+		}
+	}
+
+	void WriteInFile(Auto& a) {
+		ifstream inFile("auto.txt");
+		if (inFile.is_open()) {
+			inFile >> a;
+			cout << "Brand: " << a.getBrand() << "\nSpeed : " << a.getSpeed() << "\nYear of presentation : " <<a.getYear() << "\nVes : " << a.getVes() << endl;
+			inFile.close();
+		}
+	}
+
+
+
+	friend ofstream& operator<<(ofstream& os, const Auto& a) {
+		os << "Brand: " << a.brand << "\nSpeed : " << a.speed << "\nYear of presentation : " << a.year << "\nVes : " << a.ves << endl;
+		return os;
+	}
+
+	friend ifstream& operator>>(ifstream& is, Auto& a) {
+		is >> a.brand >> a.speed >> a.year >> a.ves;
+		return is;
 	}
 
 
@@ -34,18 +84,14 @@ public:
 int main()
 {
 	Auto obj;
-	ofstream outFile("auto.txt");
-	if (outFile.is_open()) {
-		outFile << obj;
-		outFile.close();
-	}
 
-	Auto obj2;
-	ifstream inFile("auto.txt");
-	if (outFile.is_open()) {
-		outFile >> obj2;
-		cout  << "Brand: " << obj2.brand << "\nSpeed : " << obj2.speed << "\nYear of presentation : " << obj2.year << "\nVes : " << obj2.ves << endl;
-		outFile.close();
-	}
+	obj.WriteInFile(obj);
+	obj.writeOutFile(obj);
+	
+
+	Auto obj2 = Auto("BMW M5" , 240 , 20219 , 5);
+
+	obj2.WriteInFile(obj2);
+	obj2.writeOutFile(obj2);
 	
 }
